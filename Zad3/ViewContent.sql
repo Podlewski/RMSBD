ea842770a9f4ff2fed96de3bc53f3c838718d026
Bartek Jurczewski <bartek.jurczewski@gmail.com>
2020-04-17 14:21:57 +0200

    View three tables in xml

--- Zad3/ViewContent.sql
+++ Zad3/ViewContent.sql
@@ -1,7 +1,7 @@
 USE kino
 GO
 
-CREATE OR ALTER PROCEDURE ExportWholeTable
+CREATE OR ALTER PROCEDURE ViewAktorzy
 AS
 BEGIN
     SELECT id_aktor, nazwisko, data_urodzenia, narodowosc FROM aktor
@@ -9,5 +9,33 @@ BEGIN
     ('Aktor'), ROOT
     ('Aktorzy')
 END
+GO
 
-exec ExportWholeTable
+CREATE OR ALTER PROCEDURE ViewFilmy
+AS
+BEGIN
+    SELECT id_film, tytul, rok_powstania, gatunek FROM film
+    FOR XML PATH
+    ('Film'), ROOT
+    ('Filmy')
+END
+GO
+
+CREATE OR ALTER PROCEDURE ViewRole
+AS
+BEGIN
+    SELECT id_rola, id_aktor, id_film FROM rola
+    FOR XML PATH
+    ('Rola'), ROOT
+    ('Role')
+END
+GO
+
+exec ViewAktorzy
+GO
+
+exec ViewFilmy
+GO
+
+exec ViewRole
+GO
