1cd5a2cade1629ca24d81ab64aaa1cfbb7ec19e4
Karol Podlewski <podlewski@hotmail.com>
2020-06-01 12:00:04 +0200

    [ADD] Draft for database setup

---
+++ Zad4/0setup_database.sql
@@ -0,0 +1,21 @@
+IF EXISTS(SELECT 1 FROM master.dbo.sysdatabases WHERE NAME = 'kina_w_lodzi') 
+    DROP DATABASE kina_w_lodzi;
+GO
+
+CREATE DATABASE kina_w_lodzi;
+GO
+
+USE kina_w_lodzi; 
+GO
+
+CREATE TABLE kino
+( 
+    id_kino     INTEGER NOT NULL PRIMARY KEY IDENTITY(1,1), 
+    nazwa       VARCHAR(50),
+    sale        INTEGER,
+    sale_3d     INTEGER,
+    ulica       VARCHAR(100),
+    nr_budynku  VARCHAR(10),
+    nr_lokalu   VARCHAR(10)
+) 
+GO
